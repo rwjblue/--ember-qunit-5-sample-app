895b46de25f1a5eec90b40ef1917ada0886ad423
Robert Jackson <rjackson@linkedin.com>
2020-10-06 19:22:44 -0400

    Integrate simplabs/qunit-dom#816.

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -2,7 +2,9 @@ import Application from 'ember-qunit-beta-test/app';
 import config from 'ember-qunit-beta-test/config/environment';
 import { setApplication } from '@ember/test-helpers';
 import { start } from 'ember-qunit';
+import { setup } from 'qunit-dom';
 
+setup();
 setApplication(Application.create(config.APP));
 
 start();
