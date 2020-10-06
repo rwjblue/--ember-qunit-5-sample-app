d7c59e06b3f5dd97580f643de9f54615cbb2c30f
Robert Jackson <rjackson@linkedin.com>
2020-10-06 19:31:09 -0400

    Swap to passign QUnit to qunit-dom's setup.

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,10 +1,11 @@
 import Application from 'ember-qunit-beta-test/app';
 import config from 'ember-qunit-beta-test/config/environment';
 import { setApplication } from '@ember/test-helpers';
+import * as QUnit from 'qunit';
 import { start } from 'ember-qunit';
 import { setup } from 'qunit-dom';
 
-setup();
+setup(QUnit);
 setApplication(Application.create(config.APP));
 
 start();
